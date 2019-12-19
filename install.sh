f52d05b56a9c31ddd5cb89656b54cb769ceb570f
znoxx <me@znoxx.me>
2019-12-19 18:21:35 +0000

    ÃÃAdded installer

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+echo "This script installs and configures TBNG-WRT"
+
+opkg remove tor
+rm -rf /etc/tor
+opkg update
+opkg install tor kmod-ipt-nat-extra
+cp torconf/torrc.tbng-wrt /etc/
+echo "%include /etc/torrc.tbng-wrt" >> /etc/tor/torrc
+mkdir -p /usr/local/bin
+cp scripts/* /usr/local/bin/
+chmod +x /usr/local/bin/tbng*.sh
+cp -r ./luci/* /usr/lib/lua/luci/
+rm -rf /var/luci-modulecache/*
+/etc/init.d/uhttpd restart
+/etc/init.d/tor restart
+
+echo "********** Installation Finished ***********"
+echo "Edit /usr/local/bin/tbng_tor.sh and set INTERFACE= to your lan interface!"
+echo "Most probably it's already set correctly, but must be modified according to your system settings."
+echo "E.g. in VirtualBox OpenWRT interface is called 'mng' instead of 'lan'"
+echo "********************************************"
+
--- scripts/tbng_tor.sh
+++ scripts/tbng_tor.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+###Edit name of local net interface.
+###On some systems (e.g. virtualbox) it is called 'mng' instead of 'lan'
+INTERFACE=lan
+
+
 /usr/local/bin/tbng_direct.sh
-DEVICE=`ifstatus lan |  jsonfilter -e '@["device"]'`
-IP=`ifstatus lan |  jsonfilter -e '@["ipv4-address"][0].address'`
+DEVICE=`ifstatus ${INTERFACE} |  jsonfilter -e '@["device"]'`
+IP=`ifstatus ${INTERFACE} |  jsonfilter -e '@["ipv4-address"][0].address'`
 iptables -t nat -A PREROUTING -i ${DEVICE} -p udp --dport 53 -j REDIRECT --to-ports 9053 -m comment --comment TBNG-WRT
 iptables -t nat -A PREROUTING -i ${DEVICE} '!' -d ${IP}/24 -p tcp --syn -j REDIRECT --to-ports 9040 -m comment --comment TBNG-WRT
 
